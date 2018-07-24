5d34aa4662a2c6b89b0c4fbeb67bf9e3b8c78dd4
Xu-wenchao <1562654193@qq.com>
2018-07-24 15:17:27 +0800

    1

--- src/main/java/com/dcits/base/DcitsManagerSystemApplication.java
+++ src/main/java/com/dcits/base/DcitsManagerSystemApplication.java
@@ -1,6 +1,5 @@
 package com.dcits.base;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
